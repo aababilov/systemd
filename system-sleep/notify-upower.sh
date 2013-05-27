ab65248cc8348a4b309e6cbff555d829803c902c
Alessio Ababilov <ilovegnulinux@gmail.com>
2013-05-27 08:49:04 +0300

    Initial commit

---
+++ system-sleep/notify-upower.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+[ "$1" = "post" ] && exec /usr/bin/dbus-send	\
+	--system --type=signal			\
+	--dest=org.freedesktop.UPower		\
+	/org/freedesktop/UPower			\
+	org.freedesktop.UPower.Resuming
+exit 0
